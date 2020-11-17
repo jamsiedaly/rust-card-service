5e38ae45ad4c5d1479f552e79c68436c6e6fbe55
etiescu <tiernanscully@gmail.com>
2020-11-17 22:03:56 +0100

    ramping up to 100 users

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 import http from 'k6/http';
+import { check } from 'k6';
 
 export let options = {
   stages: [
-    { duration: '30s', target: 100 }
+    { duration: '10s', target: 10 },
+    { duration: '20s', target: 100 },
+    { duration: '30s', target: 100 },
+//    { duration: '30s', target: 100 },
+//    { duration: '20s', target: 10 },
+    { duration: '20s', target: 0 }
   ],
 };
 
 export default function () {
-  var url = 'http://localhost:8080/api/card';
+  var url = 'https://rust-card-service.azurewebsites.net/api/card';
   var payload = JSON.stringify({
     number: '4640207097262595',
     ccv: '123',
@@ -19,6 +25,11 @@ export default function () {
       'Content-Type': 'application/json',
     },
   };
-  http.post(url, payload, params);
+
+  var res = http.post(url, payload, params);
+  check(res, {
+    'response code was 200': (res) => res.status == 200,
+    'card number is valid': (res) => res.body == "valid",
+  });
 }
 
